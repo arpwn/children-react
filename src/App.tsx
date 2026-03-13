60b67640ea6e1c428d0a1e24d00b17ca3ec19bbf
Armandeux <armando@MacBook-Air-de-Jesus.local>
2026-03-13 13:40:39 -0600

    button added

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,16 @@ import Card from "./components/Card";
 // Componente padre
 function App () {
   return (
-    <Card>
-      <h2>Título</h2>
-      <p>Descripción del contenido</p>
-    </Card>
+    <>
+      <Card>
+        <h2>Título</h2>
+        <p>Descripción del contenido</p>
+      </Card>
+
+      <Card>
+        <button>Guardar</button>
+      </Card>
+    </>
   );
 }
 
